f6b05b420ab23ab74f79bbec976a60ff720ed5a3
braind1 <briancdurland@gmail.com>
2023-03-19 10:26:37 -0600

    Finished implementation of x-wing technique, needs debugging

--- grid.py
+++ grid.py
@@ -26,7 +26,8 @@ class Grid(QGraphicsRectItem, QObject):
                                                  self.full_grid_lone_candidates,
                                                  self.naked_pair_full_grid,
                                                  self.pointing_pairs_full_grid,
-                                                 self.bi_value_graveyard]
+                                                 self.bi_value_graveyard,
+                                                 self.x_wing]
         # create a variable to track the number of times a solving technique was used
         self.num_iterations: int = 0
         # create a list of line items for the lines in between cells
@@ -434,7 +435,7 @@ class Grid(QGraphicsRectItem, QObject):
         # iterate through all cells in the grid
         for cell in self.cells:
             # check if the cell's house position (row or column) is the position of interest (1 - 9)
-            if cell.position[house] == position_of_interest:
+            if cell.position[house] == position_of_interest and len(cell.candidates) > 0:
                 # create a list that will have the cell and its candidates
                 _position_and_candidates: List[Any] = [cell]
                 # extend the candidates of the cell to the list of the cell and its candidates
@@ -465,18 +466,29 @@ class Grid(QGraphicsRectItem, QObject):
         return None
 
     # define a function to remove candidates after x-wings have been found
-    def x_wing_remove(self, house: int, position: int):
+    def x_wing_remove(self, house: int, first_position: int, second_position: int):
         # get the candidate and 2 row or column numbers from the x wing
-        candidate, first_house, second_house = self.single_house_x_wing(house, position)[0:2]
-        # TODO: finish this function
+        candidate, first_house, second_house = self.single_house_x_wing(house, first_position)[0:2]
+        # iterate through all cells
+        for cell in self.cells:
+            # check if the cell is in the same house outputted by the house function but not in the same house given
+            if cell.position[1 - house] == (first_house or second_house) and cell.position[house] != (first_position or second_position):
+                # remove the candidate from the cell
+                cell.candidates.remove(candidate)
+                # hide the displayed candidate text item
+                cell.display_candidates[candidate - 1].hide()
 
     # define a recursive function to compare 2 rows or columns to find x-wing pairs
     def x_wing_compare(self, house: int, starting_index: int):
         # iterate through all the rows below or columns right of the current row or column
         for position in range(starting_index + 1, 10):
-            # check if the candidate and both rows or columns match
-            if self.single_house_x_wing(house, starting_index)[0:2] == self.single_house_x_wing(house, position)[0:2]:
-                # TODO: removal function for the candidate
+            # check if the candidate and both rows or columns match and have x-wing candidates
+            if self.single_house_x_wing(house, starting_index) is None:
+                continue
+            # TODO: debug line 489 - type error 'None' is not subscriptable
+            elif self.single_house_x_wing(house, starting_index)[0:2] == self.single_house_x_wing(house, position)[0:2]:
+                # call the removal function
+                self.x_wing_remove(house, starting_index, position)
                 break
         # recurse while there is still at least two more rows or columns to compare
         while starting_index < 8:
